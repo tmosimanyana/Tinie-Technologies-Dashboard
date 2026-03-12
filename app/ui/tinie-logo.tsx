6cf4b1635b9fa38c086ba6051a2e71bbdea2e89a
Tinny Mosimanyana <mbothepha@gmail.com>
2026-03-13 00:00:19 +0200

    Refactor RootLayout component to TinieLogo with updated styling and structure

--- app/ui/tinie-logo.tsx
+++ app/ui/tinie-logo.tsx
@@ -1,19 +1,11 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from './fonts';
 
-export const metadata = {
-  title: 'Tinie Technologies Dashboard',
-  description: 'Business dashboard for Tinie Technologies',
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function TinieLogo() {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <div className={`${lusitana.className} flex items-center gap-2 text-white`}>
+      <GlobeAltIcon className="h-10 w-10 rotate-[15deg]" />
+      <p className="text-[32px]">Tinie Technologies</p>
+    </div>
   );
 }
