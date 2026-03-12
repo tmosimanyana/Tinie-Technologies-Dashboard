23c29bfaf0e18c349ac3ab85dd6e3262b84b444b
Tinny Mosimanyana <mbothepha@gmail.com>
2026-03-12 23:47:06 +0200

    Replace AcmeLogo with TinieLogo in page and sidenav components; add new TinieLogo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import TinieLogo from '@/app/ui/TinieLogo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -6,13 +6,13 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52">
-        <AcmeLogo>Tinie Technologies</AcmeLogo>
+        <TinieLogo />
       </div>
 
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            <strong>Welcome to Tinie Technologies.</strong>  
+            <strong>Welcome to Tinie Technologies.</strong>
             This dashboard helps you manage your business operations,
             including customers, invoices, and revenue in one place.
           </p>
